8f589f16bb9f64e477ef72f9daa001247e38b4fd
Facundo Ayosa <80654651+Facundo-Ayosa@users.noreply.github.com>
2022-07-20 17:14:49 -0300

    React Web App Only "Portfolio" done
    Pure React

--- Practice/React/portfolio/src/components/Cards/Cards.js
+++ Practice/React/portfolio/src/components/Cards/Cards.js
@@ -4,28 +4,28 @@ import './Card.css'
 
 
 function Card(props) {
-    const svgobj = {
-        html: "../Icons/AkarIconsHtmlFill.svg",
-        css: "../Icons/AkarIconsCssFill.svg",
-        htmltable: "../Icons/html-table.svg"
+    const badge = {
+        html: "../resources/icons/html.svg",
+        css: "../resources/icons/css.svg",
+        htmltable: "../resources/icons/html-table.svg"
+    }
+    const portfolio = {
+        balancesheet: "../resources/images/balancesheet.png",
+        jsdocumentation: "../resources/images/jsdocumentation.png",
+        productlandingpage: "../resources/images/productlandingpage.png"
     }
     let badgelist = props.badges.map((src) => {
-        return <img src={svgobj[src]}/>
+        return <img src={badge[src]} className="badge"/>
     });
-    class Card extends React.Component {
-        render() {
-            return <a href="#" id="card">
+    return <a href="#" id="card">
                 <div className="badges invert">{badgelist}</div>
-                <img src={props.image} alt=""/>
+                <img src={portfolio[props.title.replace(/\s+/g, '').toLowerCase()]} alt=""/>
                 <div className="content">
                     <h3 className="project-title no-margin">{props.title}</h3>
                     <p className="origin no-margin">{props.origin}</p>
                     <p className="no-margin">{props.description}</p>
                 </div>
             </a>
-        }
-    }
-    return <Card/>
 }
 
 export default Card;
--- Practice/React/portfolio/src/components/NavBar/NavBar.js
+++ Practice/React/portfolio/src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ function NavBar() {
         <nav>
           <ul>
             <li className="nav-link"><a href="#about">About</a></li>
+            <li className="nav-link"><a href="#projects">Blog</a></li>
             <li className="nav-link"><a href="#projects">Portfolio</a></li>
             <li className="nav-link"><a href="#contact">Contact</a></li>
           </ul>
